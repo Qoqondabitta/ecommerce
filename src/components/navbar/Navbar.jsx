7f1cebe64ea04d93777883c6e4ac07d5f1ebfbd4
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2024-01-24 18:19:02 +0500

    ecommerce start

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { Container, Wrapper } from './style'
+import React from "react";
+import { Container, Input, Parts, SearchContainer, Wrapper } from "./style";
+import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
   return (
-    <Container><Wrapper>nav</Wrapper></Container>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Parts>
+          <SearchContainer>
+            <CiSearch />
+            <Input type="text" placeholder="Search" />
+          </SearchContainer>
+        </Parts>
+        <Parts></Parts>
+        <Parts></Parts>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/navbar/style.js
+++ src/components/navbar/style.js
@@ -10,6 +10,29 @@ export const Wrapper = styled.div`
   justify-content: space-between;
 `;
 
+export const Parts = styled.div`
+  /* flex: 1; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  padding: 10px;
+  border-radius: 10px;
+  border: 1px solid black;
+`;
+
+export const Input = styled.input`
+  border: none;
+  outline: none;
+  width: 180px;
+`;
+
 export const Language = styled.span`
   font-size: 16px;
   font-weight: bold;
