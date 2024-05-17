b26c1c850d8d21aa9bce848f126e6e8c7606855c
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2024-05-17 22:11:37 +0500

    constants is updated

--- src/constants/pokerTextSlider.js
+++ src/constants/pokerTextSlider.js
@@ -1,9 +1,21 @@
 export const sport = [
-  ["Black T-Shirt", "Navy Tracksuit", "Black Sleeve T-Shirt", "white"],
-  ["Formal", "Formalniy", "Формальный", "white"],
-  ["Smart", "Ihcham", "Умная", "white"],
-  ["Casual", "Ko'cha", "Повседневная ", "white"],
-  ["UnderWears", "Ichki Kiyim", "Нижнее Белье", "white"],
+  {
+    scripts: ["Black T-Shirt", "Qora Mayka", "Черная Mайка"],
+    color: ["white"],
+  },
+  {
+    scripts: ["Navy Tracksuit", "To'q Ko'k Sportivka", "Темно-синий спортивкa"],
+    color: ["white"],
+  },
+  { scripts: ["Black Sleeve T-Shirt", "Ihcham", "Умная"], color: ["white"] },
+  {
+    scripts: ["Black T-Shirt With Hood", "Ko'cha", "Повседневная "],
+    color: ["white"],
+  },
+  {
+    scripts: ["Black T-Shirt", "Ichki Kiyim", "Нижнее Белье"],
+    color: ["white"],
+  },
 ];
 export const classic = [
   ["Different Colors", "Har Hil Rang", "Различные Цвета", "white"],
@@ -13,20 +25,18 @@ export const classic = [
   ["Different Colors", "Har Hil Rang", "Различные Цвета", "white"],
 ];
 export const casual = [
-  ["Different Colors", "Har Hil Rang", "Различные Цвета","yellow"],
-  ["Different Styles", "Har Hil Stil", "Различные Стили","yellow"],
-  ["Special Offers", "Alohida", "Специальные","yellow"],
-  ["Different Ages", "Har Hil Yosh", "Различные Возраст","yellow"],
-  ["Different Colors", "Har Hil Rang", "Различные Цвета","yellow"],
+  ["Different Colors", "Har Hil Rang", "Различные Цвета", "yellow"],
+  ["Different Styles", "Har Hil Stil", "Различные Стили", "yellow"],
+  ["Special Offers", "Alohida", "Специальные", "yellow"],
+  ["Different Ages", "Har Hil Yosh", "Различные Возраст", "yellow"],
+  ["Different Colors", "Har Hil Rang", "Различные Цвета", "yellow"],
 ];
 export const formal = [
-  ["For Women", "Ayollarga", "Для Женщин","yellow"],
-  ["For Men", "Erkaklarga", "Для Мужчин","yellow"],
-  ["For Kids", "Bolalarga", "Для Детей","yellow"],
-  ["For Presents", "Sovgaga", "Для Подарков","yellow"],
-  ["For Women", "Ayollarga", "Для Женщин","yellow"],
+  ["For Women", "Ayollarga", "Для Женщин", "yellow"],
+  ["For Men", "Erkaklarga", "Для Мужчин", "yellow"],
+  ["For Kids", "Bolalarga", "Для Детей", "yellow"],
+  ["For Presents", "Sovgaga", "Для Подарков", "yellow"],
+  ["For Women", "Ayollarga", "Для Женщин", "yellow"],
 ];
 
-export const textSliderData = [
-  
-]
+export const textSliderData = [];
