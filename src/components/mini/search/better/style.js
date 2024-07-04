ffe0ffd76b027d4363d46a399ff840d47632308e
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2024-07-04 18:52:54 +0500

    search part media is being damaged

--- src/components/mini/search/better/style.js
+++ src/components/mini/search/better/style.js
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-  gap: 20px;
-`;
+// export const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-start;
+//   justify-content: flex-start;
+//   gap: 20px;
+// `;
