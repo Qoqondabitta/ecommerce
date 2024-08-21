ced4c279a27a1cc9e444a4732df5719bd6690686
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2024-08-22 00:52:53 +0500

    narcotic perfumes have been added

--- src/constants/componentsContants/collection/category/category.js
+++ src/constants/componentsContants/collection/category/category.js
@@ -4,13 +4,15 @@ import { black } from "../colors/black";
 import { blue } from "../colors/blue";
 import { casualkids } from "../kids/casualkids";
 import { formalkids } from "../kids/formalkids";
+import { cytrusperfume } from "../perfume/cytrusperfume";
+import { narcoticperfume } from "../perfume/narcoticperfume";
 import { perfumecollection } from "../perfume/perfumecollection";
 import { shoes } from "../shoes";
 import { hijabforhead } from "../women/hijabhead";
 import { hijabstyleconstants } from "../women/stylehijab";
 
 export const category = [
-  { c: ["men", "erkaklar", "Мужчины"], l: perfumecollection },
+  { c: ["men", "erkaklar", "Мужчины"], l: narcoticperfume },
   { c: ["suits", "kastyumlar", "костюмы"], l: cardfrank },
   { c: ["sport", "sport", "спорт"], l: collectioncardsport },
   { c: ["shoes", "oyoq kiyim", "обувь"], l: shoes },
