e5c11b101318acd15be7c985fc29fc42792ae40b
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2024-10-30 18:11:15 +0500

    after some3 minutes Allah Fixed my problems and now Q&A page has been updated

--- src/components/faqs/Faqs.jsx
+++ src/components/faqs/Faqs.jsx
@@ -19,12 +19,8 @@ const Faqs = () => {
   const [show, setShow] = useState(false);
   const l = useSelector((store) => store.language.value);
   const changeShow = (id, falseId=100) => {
-    console.log(newId==id, falseId);
-    id == setNewId ? setNewId(falseId) : setNewId(id);
-    console.log(id, "id");
-    // id === setNewId ? setShow(false) : setShow(true);
+    setNewId(newId==id?null:id)
   };
-  console.log(newId, "newId");
   return (
     <>
       <Main>
@@ -33,21 +29,21 @@ const Faqs = () => {
           <QuestionWrapper>
             {faqs.map((v, i) => (
               <Wrapper key={i}>
-                <Question onClick={() => changeShow(i)}>
+                <Question
+                  id={`${newId == i ? "question" : null}`}
+                  onClick={() => changeShow(i)}
+                >
                   {l == "ENG" ? v.q[0] : l == "UZB" ? v.q[1] : v.q[2]}
                   <IoIosArrowDown
                     color="black"
-                    // onClick={() => changeShow(i)}
                     className={`${
                       newId == i ? "rot" : "arrow"
                     } starting-position`}
                   />
                 </Question>
-                {newId == i && (
-                  <Answer id={`${newId == i ? "answer" : "not-answer"}`}>
-                    {l == "ENG" ? v.a[0] : l == "UZB" ? v.a[1] : v.a[2]}
-                  </Answer>
-                )}
+                <Answer id={`${newId == i ? "answer" : "not-answer"}`}>
+                  {l == "ENG" ? v.a[0] : l == "UZB" ? v.a[1] : v.a[2]}
+                </Answer>
               </Wrapper>
             ))}
           </QuestionWrapper>
